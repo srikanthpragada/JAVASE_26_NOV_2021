251a030b7833dd548b402427c6c59fccf973b1ab
srikanthpragada <srikanthpragada@gmail.com>
2021-12-03 20:55:46 +0530

    03-DEC

---
+++ demo/src/oop/TestProduct.java
@@ -0,0 +1,25 @@
+package oop;
+
+class Product {
+	// Instance variables 
+	private String name;
+	private double price;
+	
+	// Constructor 
+	public Product(String n, double p) {
+		name = n;
+		price = p;
+	}
+	// Methods
+}
+
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+      Product p1 = new Product("Dell XPS", 200000);  // object 	
+       
+
+	}
+
+}
