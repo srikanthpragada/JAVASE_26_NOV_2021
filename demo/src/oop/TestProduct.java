aee99f5315bd2628e36266bc0b2989d84ce128a5
srikanthpragada <srikanthpragada@gmail.com>
2021-12-04 20:57:24 +0530

    04-DEC

--- demo/src/oop/TestProduct.java
+++ demo/src/oop/TestProduct.java
@@ -1,24 +1,38 @@
 package oop;
 
 class Product {
-	// Instance variables 
+	// Instance variables
 	private String name;
 	private double price;
-	
-	// Constructor 
+
+	// Constructor
 	public Product(String n, double p) {
 		name = n;
 		price = p;
 	}
-	// Methods
-}
 
+	// Methods
+	public void print() {
+		System.out.println(name);
+		System.out.println(price);
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public double getNetPrice() {
+		return price + price * 0.12;
+	}
+}
 
 public class TestProduct {
 
 	public static void main(String[] args) {
-      Product p1 = new Product("Dell XPS", 200000);  // object 	
-       
+		Product p1 = new Product("Dell XPS", 200000); // object
+		// Product p2 = new Product();
+		p1.print();
+		System.out.println(p1.getNetPrice());
 
 	}
 
