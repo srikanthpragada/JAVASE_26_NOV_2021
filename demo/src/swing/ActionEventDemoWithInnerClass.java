837f8c7b02a3a9763f86a41f1a7e1a7f65cba9e3
srikanthpragada <srikanthpragada@gmail.com>
2022-01-07 20:55:16 +0530

    07-JAN

--- demo/src/swing/ActionEventDemoWithInnerClass.java
+++ demo/src/swing/ActionEventDemoWithInnerClass.java
@@ -9,35 +9,36 @@ import java.util.Date;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
- 
-public class ActionEventDemoWithInnerClass extends JFrame  {
-	JButton btnShow;
-	JLabel lblMessage;
-	
+
+public class ActionEventDemoWithInnerClass extends JFrame {
+	private JButton btnShow;
+	private JLabel lblMessage;
+
 	public ActionEventDemoWithInnerClass() {
 		super("ActionEvent Demo with inner class");
-		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		lblMessage = new JLabel();
 		lblMessage.setHorizontalAlignment(JLabel.CENTER);
-		
+
 		btnShow = new JButton("Show Date and Time");
-		
+
+		// Anonymous inner class
 		btnShow.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-                   lblMessage.setText( new Date().toString());
+				lblMessage.setText(new Date().toString());
 			}
 		});
-		
+
 		Container c = getContentPane();
 		c.add(btnShow, BorderLayout.PAGE_END);
 		c.add(lblMessage, BorderLayout.PAGE_START);
 	}
 
 	public static void main(String[] args) {
-	    ActionEventDemoWithInnerClass f = new ActionEventDemoWithInnerClass();
-	    f.setSize(400,200);
-	    f.setVisible(true);
+		ActionEventDemoWithInnerClass f = new ActionEventDemoWithInnerClass();
+		f.setSize(400, 200);
+		f.setVisible(true);
 
 	}
 
